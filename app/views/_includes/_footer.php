04e9f66fe08aea3635e371797368eac285c30573
victor-iyiola <javafolabi@gmail.com>
2017-05-31 11:18:12 +0100

    Included jQuery and tether in assets/js/

--- app/views/_includes/_footer.php
+++ app/views/_includes/_footer.php
@@ -3,6 +3,10 @@
     <p> Copyright ©2017. Victor I. Afolabi. All rights reserved.</p>
   </footer>
 
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write("<script src='<?= ASSET_PATH . 'js/jquery.min.js';
+  ?>''><\/script>")</script>
+  <script src="<?= ASSET_PATH . 'js/tether.min.js'; ?>"></script>
   <script src="<?= ASSET_PATH . 'js/bootstrap.min.js'; ?>"></script>
   <?php
   if ( isset($this->js) )

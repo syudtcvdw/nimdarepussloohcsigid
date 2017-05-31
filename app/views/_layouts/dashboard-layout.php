266dda7390674c354dc46b5d9c397f7a46aa877d
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-05-31 20:35:42 +0100

    ignored sass-cache

--- app/views/_layouts/dashboard-layout.php
+++ app/views/_layouts/dashboard-layout.php
@@ -1,12 +1,12 @@
 <?php require_once $this->header; ?>
     <div class="wrapper">
-      
+
         <div class="left  transition col-md-3 col-sm-3 slide-in-sidebar">
-            <?php require_once $this->sidebar;  ?>
+            <?php require_once $this->sidebar; ?>
         </div>
-        
+
         <div class="right col-md-9 col-sm-9 col-xs-12">
-            <?php require_once $viewFile;  ?>
+            <?php require_once $viewFile; ?>
         </div>
 
     </div>

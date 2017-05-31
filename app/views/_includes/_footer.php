c9c020af159449944d38dc63399da0a0fbd01b95
victor-iyiola <javafolabi@gmail.com>
2017-05-31 11:23:35 +0100

    Updated tether and jquery includes

--- app/views/_includes/_footer.php
+++ app/views/_includes/_footer.php
@@ -4,8 +4,7 @@
   </footer>
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write("<script src='<?= ASSET_PATH . 'js/jquery.min.js';
-  ?>''><\/script>")</script>
+  <script>window.jQuery || document.write("<script src='<?= ASSET_PATH . 'js/jquery.min.js'; ?>''><\/script>")</script>
   <script src="<?= ASSET_PATH . 'js/tether.min.js'; ?>"></script>
   <script src="<?= ASSET_PATH . 'js/bootstrap.min.js'; ?>"></script>
   <?php

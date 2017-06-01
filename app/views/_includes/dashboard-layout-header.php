fe7e0abbdcc47f19a486217b879019820ce07cf8
olubusayo52 <amoweolubusayo@gmail.com>
2017-06-01 15:17:43 +0100

    create-school modified

--- app/views/_includes/dashboard-layout-header.php
+++ app/views/_includes/dashboard-layout-header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="<?= ASSET_PATH . 'css/bootstrap-grid.min.css'; ?>">-->
     <link rel="stylesheet" href="<?= ASSET_PATH . 'css/style.css'; ?>">
     <link rel="stylesheet" href="<?= ASSET_PATH . 'css/dashboard.css'; ?>">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Open+Sans:300,400" rel="stylesheet">
+
     <?php
     if ( isset($this->css) )
         foreach ($this->css as $css)

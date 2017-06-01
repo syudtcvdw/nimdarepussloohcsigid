dafd456702041a10a50ab134b7f95b6dcabd4279
DotunLonge <funnica4@gmail.com>
2017-06-01 16:07:44 +0100

    pull: dashboard v3 commit

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,17 +1,23 @@
 $(function(){
-   $buttonSpan = $(".left button span");
+
     $left = $(".left");
     $right = $(".right");
 
 $(".showSidebar").click(function(){
-
     $left.toggleClass("toggle-this");
+});
 
-    });
     
-    $left.hover(function(){
+$left.hover(function(){
       $left.toggleClass("hover-left-width");
       $right.toggleClass("hover-right-width");
-    });
+});
 
+
+
+    $(".sidebar-row").click(function(){
+         $(".sidebar-row").removeClass("active");
+        $(this).toggleClass("active");
+
+    })
 });

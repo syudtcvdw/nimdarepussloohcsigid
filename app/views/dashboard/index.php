c5942a79ce51a0195ac4330fafa075d438a76d1a
DotunLonge <funnica4@gmail.com>
2017-06-01 14:28:27 +0100

    pull: admin dashboard v2.0 commit

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,55 +1,55 @@
 <div class="content">
-    <nav class=' nav navbar right-nav'>
-        <div class="container-fluid  center-things">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
+               <nav class=' nav navbar right-nav'>
+                       <div class="container-fluid  center-things">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
 
-                <button type="button" class="navbar-toggle showSidebar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/">Admin Dashboard</a>
-            </div>
+                            <button type="button" class="navbar-toggle showSidebar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="/">Admin Dashboard</a>
+                        </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="my-navbar">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="my-navbar">
 
-                <!--
-                            <ul class="nav navbar-nav navbar-right">
-                                <li>
-                                <a href="#" class="btn">Logout</a>
-                                </li>
-                                <li>
-                                <a href="" class="btn">Sign In</a>
-                                </li>
-                            </ul>
-                -->
-            </div>
-        </div>
-    </nav>
+<!--
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                <a href="#" class="btn">Logout</a>
+                </li>
+                <li>
+                <a href="" class="btn">Sign In</a>
+                </li>
+            </ul>
+-->
+                           </div>
+                   </div>
+               </nav>
 
-    <div class="body-content">
-        <div class="col-md-3">
-            <div class="info">
-                Lorem
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="info">
-                Lorem
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="info">
-                Lorem
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="info">
-                Lorem
-            </div>
-        </div>
-    </div>
-</div>
+               <div class="body-content">
+                   <div class="col-md-3">
+                       <div class="info">
+                           Lorem
+                       </div>
+                   </div>
+                   <div class="col-md-3">
+                       <div class="info">
+                           Lorem
+                       </div>
+                   </div>
+                   <div class="col-md-3">
+                       <div class="info">
+                           Lorem
+                       </div>
+                   </div>
+                   <div class="col-md-3">
+                       <div class="info">
+                           Lorem
+                       </div>
+                   </div>
+               </div>
+           </div>

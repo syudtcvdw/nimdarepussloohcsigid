a54bbe3ceaf78a21c89e3bc19491e05ca205ac7a
olubusayo52 <amoweolubusayo@gmail.com>
2017-06-01 16:50:02 +0100

    pull:create school page modified

--- app/views/dashboard/create-school.php
+++ app/views/dashboard/create-school.php
@@ -1,7 +1,7 @@
 <div class="wrapper create-school">
     <div class="body-content">
         <div class="heading">
-            <h2 class="heading2"> Create School </h2>
+            <span> Enter School Details </span>
         </div>
         <br/>
         <div class="col-sm-6 col-sm-offset-3 change-input">

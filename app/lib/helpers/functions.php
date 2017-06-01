fe80d04550ff7d13ec6c2d396c9e38eb08932eb0
victor-iyiola <javafolabi@gmail.com>
2017-06-01 12:07:17 +0100

    Hash alorithm helper function

--- app/lib/helpers/functions.php
+++ app/lib/helpers/functions.php
@@ -17,3 +17,24 @@ function redirect($uri) {
     echo '<script>window.location="' . PROJECT_PATH . $uri . '"</script>';
     die();
 }
+
+
+/**
+ * Hash a password using PASSWORD_BCRYPT default algorithm
+ * @param $password
+ * @param int $algorithm
+ * @return bool|string
+ */
+function hash($password, $algorithm=PASSWORD_BCRYPT) {
+  return password_hash($password, $algorithm);
+}
+
+/**
+ * Verifies hashed password
+ * @param $password
+ * @param $hash
+ * @return bool
+ */
+function verify_hash($password, $hash) {
+  return password_verify($password, $hash);
+}

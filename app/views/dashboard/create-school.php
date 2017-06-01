e8a828a7886e7ffc1fd3c6d6a55797f36af8951d
olubusayo52 <amoweolubusayo@gmail.com>
2017-06-01 14:33:53 +0100

    create-school modified

--- app/views/dashboard/create-school.php
+++ app/views/dashboard/create-school.php
@@ -1,15 +1,21 @@
-<div class="wrapper">
-    <div class="heading">
-        <h2 class="heading2"> Create School </h2>
-    </div>
+<div class="wrapper create-school">
     <div class="body-content">
-        <div class="col-sm-6 col-sm-offset-2">
-            <input type="text" name="school_name" class="form-control" placeholder="Enter School Name"/>
-            <br/>
-            <input type="text" name="admin_name" class="form-control" placeholder="Admin Name"/>
+        <div class="heading">
+            <h2 class="heading2"> Create School </h2>
+        </div>
+        <br/>
+        <div class="col-sm-6 col-sm-offset-3">
+            <input type="text" name="school_name" class="form-control" placeholder="School Name"/>
             <br/>
             <input type="text" name="school_population" class="form-control" placeholder="School Population"/>
             <br/>
+            <input type="text" name="school_population" class="form-control" placeholder="School Population"/>
+            <br/>
+            <input type="text" name="admin_username" class="form-control" placeholder="Admin Username"/>
+            <br/>
+            <input type="text" name="admin_password" class="form-control" placeholder="Admin Password"/>
+            <br/>
+
             <button type="button" class="btn btn-block submit-button">Submit</button>
         </div>
     </div>
